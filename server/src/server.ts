9d57c36f4ba9299d9e6e5876656389d8f885c794
Jordan Braz <jordanbraz@outlook.com>
2023-05-23 11:27:32 -0300

    routes add, CRUD memories

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,15 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
+app.register(jwt, {
+  secret: 'spacetime',
+})
 app.register(cors, {
   origin: true,
 })
---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,8 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com Amor no NWL da{' '}
+      <a className="undeline hover:text-gray-100">rocketseat</a>
+    </div>
+  )
+}
